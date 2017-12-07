5d6fae6b2a30ec318daf328b146d3788a6480f90
Fachri Febrian <fachripaul@gmail.com>
2017-12-07 19:42:24 +0700

    Initial Commit

---
+++ MusicLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicLibrary
+//
+//  Created by Fachri Work on 12/7/17.
+//  Copyright © 2017 Decadev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
